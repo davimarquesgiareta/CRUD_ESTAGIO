c0f9e9e70a0fc34fe4c844a0a0607d582a1a067a
Davi Marques <davimgcb@gmail.com>
2021-04-25 18:42:32 -0300

    Update part

--- frontend/frontendcrud/src/App.js
+++ frontend/frontendcrud/src/App.js
@@ -1,9 +1,12 @@
 import React, {useState, useEffect} from "react";
 import './App.css';
 import Axios from 'axios';
+var idaux=0
 
 function App() {
-
+  var subButton = document.getElementById("subButton")
+  var updateButton = document.getElementById("updateButton")
+  
   const [name, setName] = useState('')
   const [description, setDescription] = useState('')
   const [price, setPrice] = useState(0)
@@ -36,25 +39,64 @@ function App() {
     window.location.href = '/'
   }
 
+  const updateForm = (product) => {
+    
+    subButton.style.display = 'none'
+    updateButton.style.display = 'block'
+    
+    document.getElementById("name").value = product.name ;
+    document.getElementById("description").value = product.description;
+    document.getElementById("price").value = product.price ;
+    document.getElementById("quantity").value = product.quantity ;
+
+  idaux = product.id
+  setName(document.getElementById("name").value)
+  setDescription(document.getElementById("description").value)
+  setPrice(document.getElementById("price").value)
+  setQuantity(document.getElementById("quantity").value)
+    
+  window.scrollTo(0, 0);
+  }
+
+ const updateProduct = () => {
+  //Axios.put('http://localhost:4000/products/'+id) 
+  //window.location.href = '/'
+  setName(document.getElementById("name").value)
+  setDescription(document.getElementById("description").value)
+  setPrice(document.getElementById("price").value)
+  setQuantity(document.getElementById("quantity").value)
+  
+  Axios.put('http://localhost:4000/products/'+idaux,{
+        name: name,
+        description: description,
+        price: price,
+        quantity: quantity 
+    })
+  alert('sucess update')
+  window.location.href = '/'
+ }
+
   return (
     <div className="App">
       <div className="form" id="productForm">
         <h1>CRUD APLLICATION</h1>
 
         <label>Name</label>
-        <input type="text" name="name" onChange={(e) => setName(e.target.value)}/>
+        <input id="name" type="text" name="name" onChange={(e) => setName(e.target.value)}/>
 
         <label>Description</label>
-        <input type="text" name="description" onChange={(e) => setDescription(e.target.value)}/>
+        <input id="description" type="text" name="description" onChange={(e) => setDescription(e.target.value)}/>
 
         <label>Price</label>
-        <input type="number" name="price" onChange={(e) => setPrice(e.target.value)}/>
+        <input id="price" type="number" name="price" onChange={(e) => setPrice(e.target.value)}/>
 
         <label>Quantity</label>
-        <input type="number" name="quantity" onChange={(e) => setQuantity(e.target.value)} />
+        <input id="quantity" type="number" name="quantity" onChange={(e) => setQuantity(e.target.value)} />
 
-        <button onClick={submitProduct}>Submit</button>
        
+        <button id="subButton" onClick={submitProduct}>Submit</button>
+        <button id="updateButton" onClick={updateProduct}>Save</button>
+
       </div>
       {productsList.map((val)=>{
           return(
@@ -65,12 +107,18 @@ function App() {
             <p>Price: <b>{val.price} </b></p>
             <p>Quantity: <b>{val.quantity}</b></p>
             <button onClick={ (e)=> deleteProduct(val.id)}>Delete</button>
-            <input type="text"/>
-            <button>Update</button>
+            <button onClick={ (e)=> updateForm(
+              {id: val.id,
+              name: val.name, 
+              description: val.description,
+              price: val.price,
+              quantity: val.quantity}
+            )
+              }>Update</button>
             </div>
             
           ) 
-        })}
+        })} 
     </div>
   );
 }
